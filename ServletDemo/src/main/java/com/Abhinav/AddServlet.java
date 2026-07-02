9c75f516b33fd645a75d3c4f4a9438a7404248ad
08abhinav <abhinavnegi101@gmail.com>
2026-07-02 17:17:29 +0530

    Servlet to add two numbers

---
+++ ServletDemo/src/main/java/com/Abhinav/AddServlet.java
@@ -0,0 +1,21 @@
+package com.Abhinav;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		
+		PrintWriter out = res.getWriter();
+		out.println("Result: " + (a+b));
+	}
+	
+}
