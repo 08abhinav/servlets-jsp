afc2d7d7c23caa361813ffb908416cc7ff30bbfd
08abhinav <abhinavnegi101@gmail.com>
2026-07-03 10:56:19 +0530

    implemented Session management

--- ServletDemo/src/main/java/com/SessionManagement/Page1.java
+++ ServletDemo/src/main/java/com/SessionManagement/Page1.java
@@ -6,21 +6,38 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Page1 extends HttpServlet{
+	private final String tempPassword = "123456789";
 	
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
+		//Session Management
 		String name = req.getParameter("name");
-		int age     = Integer.parseInt(req.getParameter("age"));
+		String password = req.getParameter("password");
 		
+		HttpSession session = req.getSession();
 		
-		if(age >= 18) {
-			//URL rewriting
-			res.sendRedirect("page2?= " + name);
+		if(password.equals(tempPassword)) {
+			session.setAttribute("name", name);
+			res.sendRedirect("page2");
 		}else {
 			PrintWriter out = res.getWriter();
-			out.println("<h1>Hi <u>"+name+"</u>, you are not eligible to vote.</h1>");
+			out.println("<h1> Invalid Password </h1>");
 		}
+		
+//		String name = req.getParameter("name");
+//		int age     = Integer.parseInt(req.getParameter("age"));
+		
+		//URL rewriting
+//		if(age >= 18) {
+//			res.sendRedirect("page2?= " + name);
+//		}else {
+//			PrintWriter out = res.getWriter();
+//			out.println("<h1>Hi <u>"+name+"</u>, you are not eligible to vote.</h1>");
+//		}
+		
+		
 	}
 }
--- ServletDemo/src/main/java/com/SessionManagement/Page2.java
+++ ServletDemo/src/main/java/com/SessionManagement/Page2.java
@@ -6,12 +6,24 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Page2 extends HttpServlet{
-protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
-		String name = req.getParameter("name");
+		//Session Management
+		
+		HttpSession session = req.getSession();
+
+		String name = (String) session.getAttribute("name");
+		
 		PrintWriter out = res.getWriter();
-		out.println("<h1>Hi <u>"+name+"</u>, you are eligible to vote.</h1>");
+		out.println("<h1>Welcome <u>"+name+"</u></h1>");
+	
+	
+//		URL rewriting
+//		String name = req.getParameter("name");
+//		PrintWriter out = res.getWriter();
+//		out.println("<h1>Hi <u>"+name+"</u>, you are eligible to vote.</h1>");
 	}
 }
