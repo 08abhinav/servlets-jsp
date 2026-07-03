58661e5dbbe0c5ed8dfb87421f6ccd174a84a4ea
08abhinav <abhinavnegi101@gmail.com>
2026-07-03 11:08:42 +0530

    implemented Cookie

--- ServletDemo/src/main/java/com/SessionManagement/Page1.java
+++ ServletDemo/src/main/java/com/SessionManagement/Page1.java
@@ -2,7 +2,9 @@ package com.SessionManagement;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.HttpCookie;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,24 +15,40 @@ public class Page1 extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
-		//Session Management
+		//Cookies
 		String name = req.getParameter("name");
 		String password = req.getParameter("password");
 		
-		HttpSession session = req.getSession();
-		
 		if(password.equals(tempPassword)) {
-			session.setAttribute("name", name);
+			Cookie cookie = new Cookie("name", name);
+			res.addCookie(cookie);
 			res.sendRedirect("page2");
 		}else {
 			PrintWriter out = res.getWriter();
 			out.println("<h1> Invalid Password </h1>");
 		}
 		
+		
+		
+		//Session Management
+//		String name = req.getParameter("name");
+//		String password = req.getParameter("password");
+//		
+//		HttpSession session = req.getSession();
+//		
+//		if(password.equals(tempPassword)) {
+//			session.setAttribute("name", name);
+//			res.sendRedirect("page2");
+//		}else {
+//			PrintWriter out = res.getWriter();
+//			out.println("<h1> Invalid Password </h1>");
+//		}
+
+		
+		//URL rewriting
 //		String name = req.getParameter("name");
 //		int age     = Integer.parseInt(req.getParameter("age"));
 		
-		//URL rewriting
 //		if(age >= 18) {
 //			res.sendRedirect("page2?= " + name);
 //		}else {
--- ServletDemo/src/main/java/com/SessionManagement/Page2.java
+++ ServletDemo/src/main/java/com/SessionManagement/Page2.java
@@ -3,6 +3,7 @@ package com.SessionManagement;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,14 +12,32 @@ import javax.servlet.http.HttpSession;
 public class Page2 extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
+		//Cookie
+		
+		Cookie cookie[] = req.getCookies();
+
+		PrintWriter out = res.getWriter();
+		
+		if(cookie.length != 0) {
+			for(Cookie c : cookie) {
+				out.println("<h1>Welcome <u> "+c.getValue()+"</u></h1>");
+			}
+		}else {
+			out.println("<h1>Unauthenticated</h1>");
+		}
+		
+		
+		
+		
+		
 		//Session Management
 		
-		HttpSession session = req.getSession();
-
-		String name = (String) session.getAttribute("name");
-		
-		PrintWriter out = res.getWriter();
-		out.println("<h1>Welcome <u>"+name+"</u></h1>");
+//		HttpSession session = req.getSession();
+//
+//		String name = (String) session.getAttribute("name");
+//		
+//		PrintWriter out = res.getWriter();
+//		out.println("<h1>Welcome <u>"+name+"</u></h1>");
 	
 	
 //		URL rewriting
