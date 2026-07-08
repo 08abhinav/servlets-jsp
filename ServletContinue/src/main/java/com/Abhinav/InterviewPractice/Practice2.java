44bcb92b3608f2e5a55ac3661931f563a645df7c
08abhinav <abhinavnegi101@gmail.com>
2026-07-08 10:55:43 +0530

    implemented unsafe thread servletcontext

---
+++ ServletContinue/src/main/java/com/Abhinav/InterviewPractice/Practice2.java
@@ -0,0 +1,28 @@
+/*Thread safety in ServletContext*/
+package com.Abhinav.InterviewPractice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Practice2 extends HttpServlet{
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		ServletContext ctx = getServletContext();
+		Integer count = (Integer) ctx.getAttribute("count");
+		
+		if(count == null) {
+			count = 0;
+		}
+		
+		count += 1;
+		
+		ctx.setAttribute("count", count);
+		ctx.setAttribute("count", count);
+		
+		res.getWriter().println("Hits: " + count);
+	}
+}
